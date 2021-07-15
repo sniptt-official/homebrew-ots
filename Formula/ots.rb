c9751ca37dd94b9fb97535d565ba8a5e02a2ce15
Roger <roger@sniptt.com>
2021-07-15 16:48:40 +0100

    fix(formula): fix homebrew formula

--- Formula/ots.rb
+++ Formula/ots.rb
@@ -1,12 +1,17 @@
 class Ots < Formula
   desc "🔐 Share end-to-end encrypted secrets with others via a one-time URL"
   homepage "https://github.com/sniptt-official/ots-cli"
-  url "https://github.com/sniptt-official/ots-cli/releases/download/v0.0.3/ots-cli_0.0.3_darwin_amd64.tar.gz"
-  sha256 "cdc6b13582a42bc0f7f6a2c76cf11ee5181d934a5a81462fc08f90efdc5429b5"
-  version "0.0.3"
+  url "https://github.com/sniptt-official/ots-cli/releases/download/v0.0.4/ots-cli_0.0.4_darwin_amd64.tar.gz"
+  sha256 "5938b8b7b5ca729be02484c3936cb5c51d499d43206c846459f1eb75389d9410"
+  version "0.0.4"
   license "Apache-2.0"
 
   def install
     bin.install "ots-cli" => "ots"
+  end
+
+  test do
+    system "#{bin}/ots --version"
+    assert_match "ots version 0.0.4"
   end
 end
