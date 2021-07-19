c29a523fc845e6093bd72ca2075073419e0e43cf
Roger <84327906+rogerthebest@users.noreply.github.com>
2021-07-19 16:53:06 +0100

    fix(formula): fix formula

--- Formula/ots.rb
+++ Formula/ots.rb
@@ -1,7 +1,7 @@
 class Ots < Formula
   desc "🔐 Share end-to-end encrypted secrets with others via a one-time URL"
-  homepage "https://github.com/sniptt-official/ots-cli"
-  url "https://github.com/sniptt-official/ots-cli/releases/download/v0.0.5/ots-cli_0.0.5_darwin_amd64.tar.gz"
+  homepage "https://github.com/sniptt-official/ots"
+  url "https://github.com/sniptt-official/ots/releases/download/v0.0.5/ots-cli_0.0.5_darwin_amd64.tar.gz"
   version "0.0.5"
   sha256 "36131e5254e5f8d45fd88c379d8791fd37a94dc6fef5ae1b324849c5ad338a2f"
   license "Apache-2.0"
