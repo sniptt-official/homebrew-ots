12a67fac3a2441a0c5f2882cb34612f7763602cb
Slavo Vojacek <public@slavovojacek.com>
2021-07-31 20:12:08 +0100

    chore(formula): update formula to 0.0.8

--- Formula/ots.rb
+++ Formula/ots.rb
@@ -1,9 +1,9 @@
 class Ots < Formula
   desc "🔐 Share end-to-end encrypted secrets with others via a one-time URL"
   homepage "https://github.com/sniptt-official/ots"
-  url "https://github.com/sniptt-official/ots/releases/download/v0.0.7/ots_0.0.7_darwin_amd64.tar.gz"
-  version "0.0.7"
-  sha256 "0a10ed58e24cfbfee143f63db20ed607fd0dc73889d9ef979e140c8360e90e38"
+  url "https://github.com/sniptt-official/ots/releases/download/v0.0.8/ots_0.0.8_darwin_amd64.tar.gz"
+  version "0.0.8"
+  sha256 "5474e6e7f100b095276ce579eff644355d34b1a5a7b55497f60bd4433d0e02c0"
   license "Apache-2.0"
 
   def install
@@ -12,6 +12,6 @@ class Ots < Formula
 
   test do
     output = shell_output("#{bin}/ots --version")
-    assert_match "ots version 0.0.7", output
+    assert_match "ots version 0.0.8", output
   end
 end
